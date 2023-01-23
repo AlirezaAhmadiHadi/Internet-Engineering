c414724a52db0efde4741cda4d1b136a6d3b0447
Alireza <Alireza.hadi1308.aah@gmail.com>
2023-01-23 23:21:21 +0330

    Add theme to student form

--- projects/forms.py
+++ projects/forms.py
@@ -1,26 +1,33 @@
-from django.forms import ModelForm
+from django import forms
 from .models import student, course, content, registration
 
 
-class StudentForm(ModelForm):
+class StudentForm(forms.ModelForm):
     class Meta:
         model = student
-        fields = '__all__'
+        fields = ['name', 'age', 'studentNumber', 'student_image']
+
+    def __init__(self, *args, **kwargs):
+        super(StudentForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update(
+                {'class': 'input input--text'})
 
 
-class CourseForm(ModelForm):
+class CourseForm(forms.ModelForm):
     class Meta:
         model = course
         fields = '__all__'
 
 
-class ContentForm(ModelForm):
+class ContentForm(forms.ModelForm):
     class Meta:
         model = content
         fields = '__all__'
 
 
-class RegistrationForm(ModelForm):
+class RegistrationForm(forms.ModelForm):
     class Meta:
         model = registration
         fields = '__all__'
