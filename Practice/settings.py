38531839e5ffdbfa7ee6ba9cc8dafddcc999ff0c
Alireza <Alireza.hadi1308.aah@gmail.com>
2023-01-29 23:56:57 +0330

    set user config

--- Practice/settings.py
+++ Practice/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'projects.apps.ProjectsConfig'
-    'users.apps.UsersConfig'
+    'projects.apps.ProjectsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
